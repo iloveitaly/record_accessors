283c7b0a8766e9fe16dcc9c64b90b59a37ad98c3
Michael Bianco <iloveitaly@gmail.com>
2016-06-02 12:41:35 -0400

    Initial record_accessors implementation

--- lib/record_accessors.rb
+++ lib/record_accessors.rb
@@ -1,5 +1,36 @@
 require "record_accessors/version"
 
 module RecordAccessors
-  # Your code goes here...
+
+  def self.included(base)
+    base.class_eval do
+      class << self
+        def available_attributes
+          @available_attributes ||= []
+        end
+
+        alias_method :attr_accessor_without_tracking, :attr_accessor
+        def attr_accessor(*names)
+          available_attributes.concat(names)
+          attr_accessor_without_tracking(*names)
+        end
+      end
+    end
+  end
+
+  def hash_from_attributes
+    self.class.available_attributes.inject({}) do |h, key|
+      h[key] = self.send(key)
+      h
+    end
+  end
+
+  def initialize_from_attributes(attrs = {})
+    attrs.each do |name, value|
+      if respond_to?("#{name}=")
+        send("#{name}=", value)
+      end
+    end
+  end
+
 end
